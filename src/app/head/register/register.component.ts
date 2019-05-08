db05be163c68907df8a12ef6b87b3272c12884cc
knockinHeavenDoor <61239289@qq.com>
2019-05-08 21:44:45 +0800

    root font-size

--- src/app/head/register/register.component.ts
+++ src/app/head/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  @Output closeRegister = new EventEmitter();
 
   constructor() { }
 
