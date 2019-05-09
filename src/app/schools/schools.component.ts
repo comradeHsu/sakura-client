19f31179f8105a8bf97d13946e0523efdb91058d
knockinHeavenDoor <61239289@qq.com>
2019-05-09 18:09:35 +0800

    small func

--- src/app/schools/schools.component.ts
+++ src/app/schools/schools.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SchoolService} from '../service/school.service';
+import {Region} from '../model/region';
 
 @Component({
   selector: 'app-schools',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchoolsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SchoolService) { }
+
+  parentRegions: Region[];
+  subRegions: Region[];
+  parentId: any = '';
+  subId: any;
+  request: {
+    name: null,
+    parentId: null,
+    subId: null,
+    rankingTop: null,
+    rankingBottom: null
+  };
 
   ngOnInit() {
+    this.getParentRegion();
+  }
+
+  getParentRegion(): void {
+    this.service.getParentRegion().subscribe(data => {
+      this.parentRegions = data.data;
+    });
+  }
+
+  getSubRegion(): void {
+    if (this.parentId === '') {
+      this.subRegions = null;
+      return;
+    }
+    this.service.getSubRegion(this.parentId).subscribe(data => {
+      this.subRegions = data.data;
+    });
   }
 
 }
--- src/app/service/school.service.ts
+++ src/app/service/school.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ResponseResult} from '../model/response';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchoolService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getParentRegion(): Observable<ResponseResult> {
+    const url = `http://localhost:8080/region/parentRegions`;
+    return this.http.get(url).pipe(map(res => res as ResponseResult));
+  }
+
+  getSubRegion(parentId: number): Observable<ResponseResult> {
+    const url = `http://localhost:8080/region/parent/${parentId}`;
+    return this.http.get(url).pipe(map(res => res as ResponseResult));
+  }
 }
